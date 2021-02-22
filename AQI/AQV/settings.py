8752c75a12cbe0f59d31d2716dd8d007932f32bb
[nageshnayak2000] <nageshnayak2050@gmail.com>
2021-02-22 22:04:16 +0530

    boy images

--- AQI/AQV/settings.py
+++ AQI/AQV/settings.py
@@ -56,7 +56,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            os.path.join(BASE_DIR,'template')
+            os.path.join(BASE_DIR, 'template')
         ],
         'APP_DIRS': True,
         'OPTIONS': {
@@ -121,3 +121,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(
+    BASE_DIR, "static"),)
